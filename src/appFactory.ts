3bb0b3d5a052054a989e604a0cdf33e2a0a8bdbc
Mark Greenwood <mark.greenwood@measurabl.com>
2024-07-04 10:03:13 -0700

    Added tests for POST /enterData showing validation errors are misleading

--- src/appFactory.ts
+++ src/appFactory.ts
@@ -1,17 +1,36 @@
-import express from "express";
+import express, { Response } from "express";
+import Path from "path";
+import { OpenAPIBackend, Request } from "openapi-backend";
 
 export function appFactory() {
   const app = express();
 
   app.use(express.json());
 
-  app.get("/hello", (req, res) => {
-    res.send("Hello, World!");
+  const api = new OpenAPIBackend({
+    definition: Path.join(__dirname, `../public/openapi.yaml`),
+    handlers: {
+      hello: (c, req: Request, res: Response) => {
+        return res.send("Hello, World!");
+      },
+      enterData: (c, req: Request, res: Response) => {
+        const payloadErrors = !c.validation.valid
+          ? c.validation.errors
+              ?.map(error => `${error.message || "error"}: ${error.schemaPath}`)
+              .join(", ") || "Unknown error"
+          : undefined;
+
+        if (payloadErrors) {
+          return res.status(400).send({ message: payloadErrors });
+        }
+
+        return res.send(req.body);
+      },
+    },
   });
 
-  app.post("/enterData", (req, res) => {
-    res.send(req.body);
-  });
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  app.use((req, res) => api.handleRequest(req as Request, req, res));
 
   return app;
 }
---
+++ test/api.test.ts
@@ -0,0 +1,109 @@
+import supertest from "supertest";
+import { appFactory } from "../src/appFactory.js";
+
+describe("GET /hello", () => {
+  const app = appFactory();
+  const server = supertest(app);
+
+  it("responds with greeting", async () => {
+    const response = await server.get("/hello");
+    expect(response.text).toBe("Hello, World!");
+  });
+});
+
+describe("POST /enterData", () => {
+  const app = appFactory();
+  const server = supertest(app);
+
+  it("responds with data entered", async () => {
+    const requestBody = {
+      distance: {
+        value: 10,
+        unit: "MILES",
+      },
+      weight: {
+        value: 150,
+        unit: "POUNDS",
+      },
+      temperature: {
+        value: 85,
+        unit: "DEGREES_FAHRENHEIT",
+      },
+    };
+
+    const response = await server.post("/enterData").send(requestBody);
+    expect(response.body).toEqual(requestBody);
+  });
+
+  it("responds with 400 and appropriate message if the request body 'distance' is not a number", async () => {
+    const requestBody = {
+      distance: {
+        value: "ten",
+        unit: "MILES",
+      },
+      weight: {
+        value: 150,
+        unit: "POUNDS",
+      },
+      temperature: {
+        value: 85,
+        unit: "DEGREES_FAHRENHEIT",
+      },
+    };
+
+    const response = await server.post("/enterData").send(requestBody);
+    expect(response.status).toEqual(400);
+    expect(response.body).toEqual({
+      message:
+        "must be number: #/properties/requestBody/properties/distance/properties/value/type",
+    });
+  });
+
+  it("responds with 400 and appropriate message if the request body 'weight' is not a number", async () => {
+    const requestBody = {
+      distance: {
+        value: 10,
+        unit: "MILES",
+      },
+      weight: {
+        value: "one-hundred-fifty",
+        unit: "POUNDS",
+      },
+      temperature: {
+        value: 85,
+        unit: "DEGREES_FAHRENHEIT",
+      },
+    };
+
+    const response = await server.post("/enterData").send(requestBody);
+    expect(response.status).toEqual(400);
+    expect(response.body).toEqual({
+      message:
+        "must be number: #/properties/requestBody/properties/weight/properties/value/type",
+    });
+  });
+
+  it("responds with 400 and appropriate message if the request body 'temperature' is not a number", async () => {
+    const requestBody = {
+      distance: {
+        value: 10,
+        unit: "MILES",
+      },
+      weight: {
+        value: 150,
+        unit: "POUNDS",
+      },
+      temperature: {
+        value: "eighty-five",
+        unit: "DEGREES_FAHRENHEIT",
+      },
+    };
+
+    const response = await server.post("/enterData").send(requestBody);
+    expect(response.status).toEqual(400);
+    expect(response.body).toEqual({
+      message:
+        "must be number: #/properties/requestBody/properties/temperature/properties/value/type",
+    });
+  });
+});
